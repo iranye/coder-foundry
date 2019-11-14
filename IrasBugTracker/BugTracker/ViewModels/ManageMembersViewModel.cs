dc6b033dc24e9ee316e794b43a29efdc9b6b9e19
Ira Nye <iranye@gmail.com>
2019-11-14 10:06:34 -0500

    IrasBugTracker: Clean up Manage User Roles and Project Members pages

--- IrasBugTracker/BugTracker/ViewModels/ManageMembersViewModel.cs
+++ IrasBugTracker/BugTracker/ViewModels/ManageMembersViewModel.cs
@@ -29,12 +29,7 @@ namespace BugTracker.ViewModels
         public Project Project { get; set; }
         public ICollection<ManageMembersViewModel> Members { get; set; } = new HashSet<ManageMembersViewModel>();
         public ICollection<ManageMembersViewModel> NonMembers { get; set; } = new HashSet<ManageMembersViewModel>();
-
-        public ICollection<ManageMembersViewModel> AllUsers
-        {
-            get { return Members.Union(NonMembers).ToList(); }
-        }
-
+        
         public ProjectMembersViewModel()
         {}
 
