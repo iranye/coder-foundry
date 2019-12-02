ab58c40033ef31b52c8744a0b3c6ba5b5d157525
Ira Nye <iranye@gmail.com>
2019-12-02 15:19:20 -0500

    IraNye.FinancialPortal: Add Budges & Views cleanup

---
+++ IraNye.FinancialPortal/FinancialPortal.Web/Controllers/BudgetsController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FinancialPortal.Web.Helpers;
+using FinancialPortal.Web.Models;
+using Microsoft.AspNet.Identity;
+
+namespace FinancialPortal.Web.Controllers
+{
+    [Authorize]
+    public class BudgetsController : Controller
+    {
+        private ApplicationDbContext _dbContext = new ApplicationDbContext();
+
+        // GET: Budgets
+        public ActionResult Index()
+        {
+            var budgets = _dbContext.Budgets.Include(b => b.Household).Include(b => b.Owner);
+            return View(budgets.ToList());
+        }
+
+        // GET: Budgets/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Budget budget = _dbContext.Budgets.Find(id);
+            if (budget == null)
+            {
+                return HttpNotFound();
+            }
+            return View(budget);
+        }
+
+        // GET: Budgets/Create
+        public ActionResult Create()
+        {
+            int householdId = HelperMethods.GetCurrentUserHouseholdId().GetValueOrDefault();
+            if (householdId == 0)
+            {
+                return RedirectToAction("Index", "Households");
+            }
+            return View(new Budget{HouseholdId = householdId});
+        }
+
+        // POST: Budgets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,HouseholdId,Name,Description,TargetAmount,CurrentAmount")] Budget budget)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (String.IsNullOrWhiteSpace(userId))
+            {
+                return RedirectToAction("Index", "Households");
+            }
+            if (ModelState.IsValid)
+            {
+                budget.OwnerId = userId;
+                budget.Created = DateTime.Now;
+                _dbContext.Budgets.Add(budget);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(budget);
+        }
+
+        // GET: Budgets/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Budget budget = _dbContext.Budgets.Find(id);
+            if (budget == null)
+            {
+                return HttpNotFound();
+            }
+            return View(budget);
+        }
+
+        // POST: Budgets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,HouseholdId,Name,Description,Created,OwnerId,TargetAmount,CurrentAmount")] Budget budget)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Entry(budget).State = EntityState.Modified;
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(budget);
+        }
+
+        // GET: Budgets/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Budget budget = _dbContext.Budgets.Find(id);
+            if (budget == null)
+            {
+                return HttpNotFound();
+            }
+            return View(budget);
+        }
+
+        // POST: Budgets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Budget budget = _dbContext.Budgets.Find(id);
+            _dbContext.Budgets.Remove(budget);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _dbContext.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
