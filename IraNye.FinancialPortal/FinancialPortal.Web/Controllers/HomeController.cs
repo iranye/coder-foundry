daa9da62ac1a8c9acbc04cfdda7708579bbe07c5
Ira Nye <iranye@gmail.com>
2019-12-03 16:23:37 -0500

    IraNye.FinancialPortal: add new Home Dashboard & POpulate Transaction Type Migration

--- IraNye.FinancialPortal/FinancialPortal.Web/Controllers/HomeController.cs
+++ IraNye.FinancialPortal/FinancialPortal.Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FinancialPortal.Web.Models;
 using FinancialPortal.Web.ViewModels;
 
 namespace FinancialPortal.Web.Controllers
@@ -10,10 +11,47 @@ namespace FinancialPortal.Web.Controllers
     [Authorize]
     public class HomeController : Controller
     {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
             MainDashboardViewModel viewModel = new MainDashboardViewModel();
             return View(viewModel);
         }
+        
+        // Scotty Code
+        public ActionResult Dashboard()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            var currentUserHouseholdId = Helpers.HelperMethods.GetCurrentUserHouseholdId();
+            if (currentUserHouseholdId == null)
+            {
+                return RedirectToAction("Index", "Households");
+            }
+            MainDashboardViewModel viewModel = new MainDashboardViewModel();
+            viewModel.Household = _db.Households.AsNoTracking().FirstOrDefault(h => h.Id == currentUserHouseholdId);
+
+            viewModel.BankAccounts  = _db.BankAccounts.AsNoTracking().Where(b => b.HouseholdId == currentUserHouseholdId).ToList();
+            viewModel.Budgets = _db.Budgets.AsNoTracking().Where(b => b.HouseholdId == currentUserHouseholdId).ToList();
+
+            viewModel.BudgetItems.AddRange(viewModel.Budgets.SelectMany(b => b.BudgetItems));
+
+            var transactions = _db.Transactions.AsNoTracking();
+            foreach (var transaction in transactions)
+            {
+                if (viewModel.BankAccounts.Any(ba => ba.Id == transaction.BankAccountId))
+                {
+                    viewModel.Transactions.Add(transaction);
+                }
+            }
+
+            return View(viewModel);
+        }
+
+
     }
 }
--- IraNye.FinancialPortal/FinancialPortal.Web/ViewModels/MainDashboardViewModel.cs
+++ IraNye.FinancialPortal/FinancialPortal.Web/ViewModels/MainDashboardViewModel.cs
@@ -1,11 +1,6 @@
-﻿using System;
+﻿using FinancialPortal.Web.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web;
-using FinancialPortal.Web.Models;
-using Microsoft.AspNet.Identity;
 
 namespace FinancialPortal.Web.ViewModels
 {
@@ -13,41 +8,60 @@ namespace FinancialPortal.Web.ViewModels
     {
         private readonly ApplicationDbContext _db = new ApplicationDbContext();
 
-        private Household _myHousehold = null;
-        public Household MyHousehold
-        {
-            get
-            {
-                if (_myHousehold == null)
-                {
-                    var currentUserId = HttpContext.Current.User.Identity.GetUserId();
-                    if (!String.IsNullOrWhiteSpace(currentUserId))
-                    {
-                        var currentUser = _db.Users.Find(currentUserId);
-                        if (currentUser != null)
-                        {
-                            _myHousehold = _db.Households.Find(currentUser.HouseholdId);
-                        }
-                    }
-                }
+        //private Household _myHousehold = null;
+        //public Household MyHousehold
+        //{
+        //    get
+        //    {
+        //        if (_myHousehold == null)
+        //        {
+        //            var currentUserId = HttpContext.Current.User.Identity.GetUserId();
+        //            if (!String.IsNullOrWhiteSpace(currentUserId))
+        //            {
+        //                var currentUser = _db.Users.Find(currentUserId);
+        //                if (currentUser != null)
+        //                {
+        //                    _myHousehold = _db.Households.Find(currentUser.HouseholdId);
+        //                }
+        //            }
+        //        }
 
-                return _myHousehold;
-            }
-        }
+        //        return _myHousehold;
+        //    }
+        //}
 
-        public int TotalTransactions
-        {
-            get { return 50; }
-        }
+        public Household Household { get; set; }
 
+        public string HouseholdName => Household.Name;
+
+        public List<BankAccount> BankAccounts { get;set; } = new List<BankAccount>();
+        public List<Budget> Budgets {get;set; } = new List<Budget>();
+        public List<BudgetItem> BudgetItems { get;set; } = new List<BudgetItem>();
+        public List<Transaction> Transactions { get;set;} = new List<Transaction>();
+        
         public int TotalInvitations
         {
-            get { return MyHousehold?.Invitations.Count() ?? 0; }
+            get { return Household?.Invitations.Count() ?? 0; }
         }
 
         public int TotalBudgets
         {
-            get { return MyHousehold?.Budgets.Count() ?? 0; }
+            get { return Budgets.Count(); }
+        }
+
+        public int TotalBudgetItems
+        {
+            get { return BudgetItems.Count(); }
+        }
+
+        public int TotalBankAccounts
+        {
+            get { return BankAccounts.Count; }
+        }
+
+        public int TotalTransactions
+        {
+            get { return Transactions.Count; }
         }
     }
 }
