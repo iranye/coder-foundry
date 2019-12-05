61e0539f47e1a296f755579cadb5112daaeb986a
Ira Nye <iranye@gmail.com>
2019-12-05 15:05:08 -0500

    IraNye.FinancialPortal: add charts

---
+++ IraNye.FinancialPortal/FinancialPortal.Web/Controllers/ChartsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FinancialPortal.Web.Models;
+
+namespace FinancialPortal.Web.Controllers
+{
+    public class ChartsController : Controller
+    {
+        public JsonResult BarChartData()
+        {
+            var barChartData = new List<BarChart>
+            {
+                new BarChart {label = "October", value = 4324},
+                new BarChart {label = "November", value = 1234},
+                new BarChart {label = "December", value = 5678}
+            };
+            return Json(barChartData);
+        }
+    }
+}
