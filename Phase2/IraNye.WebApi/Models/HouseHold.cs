eeb68d1111a9e46fd8a7e291a0504826cbb34850
Ira Nye <iranye@gmail.com>
2019-12-11 10:56:27 -0500

    Phase2.WebApi: Add swagger/swashbuckle

--- Phase2/IraNye.WebApi/Models/HouseHold.cs
+++ Phase2/IraNye.WebApi/Models/HouseHold.cs
@@ -3,17 +3,32 @@ using System.ComponentModel.DataAnnotations;
 
 namespace IraNye.WebApi.Models
 {
+    /// <summary>
+    /// Household Data Model
+    /// </summary>
     public class Household
     {
+        /// <summary>
+        /// Household PK
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Household Name
+        /// </summary>
         [Required]
         [StringLength(255, ErrorMessage = "Name must be between {2} and {1} characters long.", MinimumLength = 1)]
         public string Name { get; set; }
 
+        /// <summary>
+        /// Household Greeting
+        /// </summary>
         [StringLength(255)]
         public string Greeting { get; set; }
 
+        /// <summary>
+        /// Household Creation Date
+        /// </summary>
         [Required]
         public DateTime Created { get; set; }
     }
---
+++ Phase2/IraNye.WebApi/Scripts/CustomerSwagger.js
@@ -0,0 +1,11 @@
+﻿
+$(document).ready(function () {
+    console.log("READY!");
+
+    $(function () {
+        console.log("Changing the icon logo!");
+        // Change default Swagger icon logo
+        $("img .logo__img").attr("src", "~/Content/api-logo.jpg");
+    });
+
+});
