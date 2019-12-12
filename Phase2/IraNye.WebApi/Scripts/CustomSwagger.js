933fe666542f2ab66f24070780f233f4f4d57d59
Ira Nye <iranye@gmail.com>
2019-12-12 16:49:24 -0500

    PHase2 updates

--- Phase2/IraNye.WebApi/Scripts/CustomSwagger.js
+++ Phase2/IraNye.WebApi/Scripts/CustomSwagger.js
@@ -1,11 +1,24 @@
 ﻿
 $(document).ready(function () {
     console.log("READY!");
+    $("#logo").html("&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ");
+    $("#logo").attr("href", "#");
+
+    $("input[name='baseUrl'], input[name='apiKey'] ").css("display", "none");
+
+    $("link[type='image/png']").attr("href", "/favicon.ico");
+
+    $("title").text("Penny Saver - API");
+
+    $("#explore").text("Rehydrate");
 
     $(function () {
         console.log("Changing the icon logo!");
         // Change default Swagger icon logo
-        $("img .logo__img").attr("src", "~/Content/api-logo.jpg");
+        $("img .logo__img").attr({
+            "src": "~/Content/api-logo.jpg",
+            "height": "20px"
+        });
     });
 
 });
